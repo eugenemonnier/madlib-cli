57ce6fdcadc82c5089fa0923fd14815743fa1852
Eugene Monnier <eugene.monnier@gmail.com>
2020-05-14 10:44:22 -0700

    working madlib

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,21 +1,25 @@
-file = open('assets/spam.txt')
-global contents
-contents = file.read()
+def get_file(selected_file):
+  file = open(selected_file)
+  contents = file.read()
+  grab_keys(contents)
 
-word_list = list()
-stop = 0
+def grab_keys(contents):
+  word_list = list()
+  stop = 0
+  for word in range(contents.count('{')):
+    start = contents.find('{', stop)
+    stop = contents.find('}', start)
+    word = contents[start : stop + 1]
+    word_list.append(word)
+  mad_lib(contents, word_list)
 
-for x in range(contents.count('{')):
-  start = contents.find('{', stop)
-  stop = contents.find('}', start)
-  word = contents[start : stop + 1]
-  word_list.append(word)
+def mad_lib(contents, word_list):
+  for x in word_list:
+    contents = contents.replace(x, input('Give me a(n) ' + x + ':'), 1)
+  print(contents)
 
-# print(word_list)
+def __main__():
+  pass
 
-mad_lib = str()
-
-for x in word_list:
-  mad_lib = contents.replace(x, input('Give me a(n) ' + x + ':'), 1)
-# contents.format(**mad_lib_dict)
-print(mad_lib)
+if __name__ == "__main__":
+  get_file('assets/spam.txt')
